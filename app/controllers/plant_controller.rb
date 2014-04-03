62e9b823fe02490ae91dc20f125a563f484d997e
hitfishking <handinmine@126.com>
2014-04-03 15:44:19 +0800

    4月3日，完善action，微调界面。

--- app/controllers/plant_controller.rb
+++ app/controllers/plant_controller.rb
@@ -10,20 +10,31 @@ class PlantController < ApplicationController
 	end
 
 	def list_plant_querying
-		recd_json = params[:q_data]
+		recvd_json = params[:q_data]
+		crit_where_json = {}
+		crit_in_json = {}
 
-		#hash数据规范化处理：1) 去掉value两端的/号；2) 将value字符串对象转换成Regexp对象；
-		recd_json.each{|key, value| recd_json[key].gsub!(/\//,'')}
-		recd_json.each{|key, value| recd_json[key]=Regexp.new(recd_json[key])}
+		#对于String类hash value(形如/乔木/)，hash数据规范化处理：1) 去掉value两端的/号；2) 将value字符串对象转换成Regexp对象；
+		recvd_json.each{ |key, value|
+			unless (/\/.*\// =~ recvd_json[key]).nil?         #处理String型Criteria
+				crit_where_json[key] = recvd_json[key].gsub(/\//,'')
+				crit_where_json[key]=Regexp.new(crit_where_json[key])
+			end
+
+			unless (/\[.*\]/ =~ recvd_json[key]).nil?        #处理Array型Criteria
+				crit_in_json[key] = recvd_json[key].gsub(/\[/,'').gsub(/\]/,'')
+				crit_in_json[key] = crit_in_json[key].split(",")                    #返回的就是数组
+			end
+		}
 
 		#实施查询，返回文档集
-		records_found = Zhong.where(recd_json)
+		records_found = Zhong.where(crit_where_json).in(crit_in_json)           #数组用in为or关系；all为and关系；
 		n_records = records_found.count
 
 
 		#循环构造返回hash
-		response_json = {}
 		content_array=[]
+		response_json = {}
 
 		n_records.times do |i|
 			tmp_j = {}
@@ -36,13 +47,17 @@ class PlantController < ApplicationController
 		response_json = {"data_s" => content_array }
 
 		puts "--------rec_json--------------"
-		puts recd_json
+		puts recvd_json
 		puts "--------n_records-------------"
 		puts n_records
+		puts "--------crit_where_json-------------"
+		puts crit_where_json
+		puts "--------crit_in_json-------------"
+		puts crit_in_json
 		puts "--------response_json-------------"
 		puts response_json
 
-		render :json => response_json.to_json
+		render :json => {"data_s" => content_array }.to_json
 
 	end
 
