e313b03f372a4188944dde94edee6572499839e1
hitfishking <handinmine@126.com>
2014-04-05 23:10:41 +0800

    4月5日，plant_show开发界面部分。

---
+++ app/models/engineering.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+class Engineering
+  include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
+
+  field :shibie_tezheng, type: String
+  field :zhongzhi_midu, type: String
+  field :miaomu_guige, type: String
+  field :sheji_jianyi, type: String
+  field :shigong_yaodian, type: String
+  field :yanghu_yaodian, type: String
+  field :cankao_jiage, type: String
+
+  embedded_in :zhong
+
+end
+
